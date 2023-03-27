bdabd9dc66bb07ff763252c67033007bc7a04e8a
melrain <yezyd92@icloud.com>
2023-03-27 14:27:35 +0800

    salting

--- app.js
+++ app.js
@@ -1,7 +1,8 @@
 //jshint esversion:6
 // require('dotenv').config();
+const bcrypt = require("bcrypt");
 
-const md5 = require("md5");
+const saltRound = 10;
 
 const express = require("express");
 
@@ -48,65 +49,81 @@ app.route("/register").get((req, res) => {
     res.render("register.ejs");
 }).post((req, res) => {
     const username = req.body.username;
-    const password = md5(req.body.password);
+    const password = req.body.password
 
-    User.findOne({ email: username }).then((result) => {
-        if (username.length > 3 && password.length > 3) {
-            if (result === null) {
+    bcrypt.hash(password, saltRound, ((err, hash) => {
+        User.findOne({ email: username }).then((result) => {
+            if (username.length > 3 && password.length > 3) {
+                if (result === null) {
 
-                const newUser = new User({
-                    email: username,
-                    password: password
-                });
+                    const newUser = new User({
+                        email: username,
+                        password: hash
+                    });
 
-                newUser.save().then(()=>{
-                    console.log("注册成功")
-                    res.render("secrets.ejs");
-                }).catch((error)=>{
-                    console.log(error);
-                    res.send(error);
-                })
+                    newUser.save().then(() => {
+                        console.log("注册成功")
+                        res.render("secrets.ejs");
+                    }).catch((error) => {
+                        console.log(error);
+                        res.send(error);
+                    })
 
+                } else {
+                    console.log("已有账号，请直接登录");
+                    res.send("已有账号，请直接登录");
+                }
             } else {
-                console.log("已有账号，请直接登录");
-                res.send("已有账号，请直接登录");
-            }
-        } else {
-            console.log("账号密码长度不对");
-            res.send("账号密码长度不对");
-        };
+                console.log("账号密码长度不对");
+                res.send("账号密码长度不对");
+            };
+        })
+
+        console.log(err);
+    }))
 
 
-    })
 });
 
 
 app.route("/login").get((req, res) => {
     res.render("login.ejs");
-}).post((req,res)=>{
+}).post((req, res) => {
     const username = req.body.username;
-    const password = md5(req.body.password);
+    const password = req.body.password;
 
-    User.findOne({email:username}).then((result)=>{
-        if(result === null){
+    User.findOne({ email: username }).then((result) => {
+        if (result === null) {
             console.log("没有该账号，请注册");
             res.send("没有该账号，请注册");
-        }else{
+        } else {
             try {
-                if(password === result.password){
-                    console.log("登陆成功");
-                    res.render("secrets.ejs");
-                }else{
-                    console.log("密码错误");
-                    res.send("密码错误");
-                }
+
+                bcrypt.compare(password, result.password, ((err, compareResult) => {
+                    if (compareResult==true) {
+                        console.log("登陆成功");
+                        res.render("secrets.ejs");
+                    } else {
+                        console.log("密码错误");
+                        res.send("密码错误");
+                    }
+                }))
+
+
+                // if(password === result.password){
+                //     console.log("登陆成功");
+                //     res.render("secrets.ejs");
+                // }else{
+                //     console.log("密码错误");
+                //     res.send("密码错误");
+                // }
             } catch (error) {
                 console.log(error);
             }
         }
-    }).catch((error)=>{
+    }).catch((error) => {
         console.log(error);
-    })
+    });
 })
 
 
